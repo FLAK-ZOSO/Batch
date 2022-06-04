649fd9ff785d210139050dab13043d32934f47b3
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-06-04 19:29:23 +0200

    ...

---
+++ PowerShell/Read-Host.ps1
@@ -0,0 +1,7 @@
+﻿$Variable = Read-Host "Enter a variable";
+echo $Variable;
+$Password = Read-Host "Enter your password" -AsSecureString;
+echo $Password;
+$Mask = Read-Host "Enter your masked string" -MaskInput;
+echo $Mask;
+pause;
